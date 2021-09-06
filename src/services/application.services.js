e3b01135799aa2e79885c4e9fa2069fc984848c8
menassieErmias <ermiasmenase@gmail.com>
2021-09-06 11:33:29 +0300

    feat: update and delete functions on application services

--- src/services/application.services.js
+++ src/services/application.services.js
@@ -19,9 +19,23 @@ async function getApplication(filter) {
   return ApplicationModel.findOne(filter);
 }
 
+async function updateApplication(id, body) {
+  return ApplicationModel.updateOne({ _id: id }, body);
+}
+
+async function deleteApplication(id) {
+  return ApplicationModel.findByIdAndDelete(id);
+}
+
+async function deleteApplications() {
+  return ApplicationModel.deleteMany();
+}
 export {
   createApplication,
   getApplications,
   getJobApplications,
   getApplication,
+  updateApplication,
+  deleteApplication,
+  deleteApplications,
 };
