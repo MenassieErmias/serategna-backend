752e70db78d13e94be6489f36e58af26fc969079
menassieErmias <ermiasmenase@gmail.com>
2021-09-06 11:13:53 +0300

    feat: application model

--- src/models/application.mongoose.js
+++ src/models/application.mongoose.js
@@ -1,31 +1,33 @@
 import mongoose from 'mongoose';
 import { STATUS } from '../constants/enums.constants.js';
 
-
-const applicationSchema = mongoose.Schema({
-    auditionPostId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'AuditionPost',
-        required: true,
+const applicationSchema = mongoose.Schema(
+  {
+    jobId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Job',
+      required: true,
     },
-    applicantId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true,
+    freelancerId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
     },
-    applicationLetter: {
-        type: String,
-        required: true,
-    }, 
+    coverLetter: {
+      type: String,
+      required: true,
+    },
     applicationStatus: {
-        type: String,
-        default: STATUS.PENDING,
-        enum: Object.values(STATUS),
-    }
-}, {
+      type: String,
+      default: STATUS.PENDING,
+      enum: Object.values(STATUS),
+    },
+  },
+  {
     timestamps: true,
-})
+  }
+);
 
-const ApplicationModel = mongoose.model('Application', applicationSchema)
+const ApplicationModel = mongoose.model('Application', applicationSchema);
 
-export default ApplicationModel
+export default ApplicationModel;
