bf1387a0d873a069d5930389d66b01cfe3ef582c
Eedecheru <edencheru07@gmail.com>
2021-09-07 09:39:27 +0300

    feat: '/:id' application routes

--- src/routes/application.route.js
+++ src/routes/application.route.js
@@ -14,5 +14,10 @@ router
   .post(errorCatcher(isAuthenticated), errorCatcher(httpCreateApplication))
   .get(errorCatcher(isAuthenticated), errorCatcher(httpGetApplications))
   .delete(errorCatcher(isAuthenticated), errorCatcher(httpDeleteApplications));
+router
+  .route('/:id')
+  .get(errorCatcher(isAuthenticated), errorCatcher(httpGetApplication))
+  .put(errorCatcher(isAuthenticated), errorCatcher(httpUpdateApplication))
+  .delete(errorCatcher(isAuthenticated), errorCatcher(httpDeleteApplication));
 
 export default router;
